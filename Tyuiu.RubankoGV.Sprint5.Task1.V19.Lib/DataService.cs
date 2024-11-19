f6313e6338db1bd224e7af7f4196327f1fa0e1b9
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-19 15:33:09 +0300

    Task1

---
+++ Tyuiu.RubankoGV.Sprint5.Task1.V19.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RubankoGV.Sprint5.Task1.V19.Lib
+{
+    public class DataService : ISprint5Task1V19
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            double y;
+            string stry;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Sin(x) + (2 / (3 * x + 5)) - 2 * Math.Cos(x) * 2 * x), 2);
+                stry = Convert.ToString(y);
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, stry + Environment.NewLine);
+                }
+                 else
+                {
+                    File.AppendAllText(path, stry);
+                }
+
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint5.Task1.V19/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.RubankoGV.Sprint5.Task1.V19.Lib;
+namespace Tyuiu.RubankoGV.Sprint5.Task1.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+            DataService ds = new DataService();
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
