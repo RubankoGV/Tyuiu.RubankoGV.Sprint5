cdfdbe6e2780d1c3ffb38604d915749250f66aae
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-28 17:27:42 +0300

    Task5

---
+++ Tyuiu.RubankoGV.Sprint5.Task5.V4/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.RubankoGV.Sprint5.Task5.V4.Lib;
+namespace Tyuiu.RubankoGV.Sprint5.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask5V4.txt";
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+
+            Console.WriteLine(" ***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
