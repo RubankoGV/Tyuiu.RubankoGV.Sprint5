92fe9dcfd2cc13f3d8fcef3c99c15e4c8faca2f7
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-28 17:06:18 +0300

    Task4

---
+++ Tyuiu.RubankoGV.Sprint5.Task4.V2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.RubankoGV.Sprint5.Task4.V2.Lib;
+namespace Tyuiu.RubankoGV.Sprint5.Task4.V2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\User\source\repos\Tyuiu.RubankoGV.Sprint5";
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
