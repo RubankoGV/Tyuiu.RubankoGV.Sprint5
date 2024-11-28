6906316b1bab6138c9012abb7441594afe6b149f
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-28 17:37:11 +0300

    Task6

---
+++ Tyuiu.RubankoGV.Sprint5.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.RubankoGV.Sprint5.Task6.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint5.Task6.V9/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.RubankoGV.Sprint5.Task6.V9.Lib;
+namespace Tyuiu.RubankoGV.Sprint5.Task6.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V25.txt");
+            Console.WriteLine("Данные находятся в файле:" + path);
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Количество элеметов длина которых 3 символа " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
