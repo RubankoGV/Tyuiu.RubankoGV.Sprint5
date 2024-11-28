8846499d5be4cacc57b6c35fe0247b50267835e1
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-28 16:53:08 +0300

    Task3

---
+++ Tyuiu.RubankoGV.Sprint5.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.RubankoGV.Sprint5.Task3.V15.Lib;
+namespace Tyuiu.RubankoGV.Sprint5.Task3.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.RubankoGV.Sprint5.Task3.V15/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Tyuiu.RubankoGV.Sprint5.Task3.V15.Lib;
+namespace Tyuiu.RubankoGV.Sprint5.Task3.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                            *");
+            Console.WriteLine("*******************************************************************************");
+            int x = 5;
+            Console.WriteLine("* Результат:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
